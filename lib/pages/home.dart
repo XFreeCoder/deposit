ca658407f6fd9a139ba59981ee7ccbfe5ad7f51a
He Linming <hlm52pk@163.com>
2023-10-23 19:14:06 +0800

    chore: clean unused code

---
+++ lib/pages/home.dart
@@ -0,0 +1,19 @@
+import 'package:deposit/main.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+        child: Center(
+      child: Text("Deposit"),
+    ));
+  }
+}
